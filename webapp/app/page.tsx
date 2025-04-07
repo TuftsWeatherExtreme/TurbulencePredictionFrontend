42e648091aa2308b17af6d9a5d6afb2bf07f081d
chrlatte <Charlotte.versavel@tufts.edu>
2025-04-07 12:45:51 -0400

    map appears in middle of page

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -6,14 +6,13 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-6 sm:p-20">
       
-    
       {/* Buttons */}
       <div className="flex gap-4 mb-4">
         <button className="px-4 py-2 bg-blue-500 text-white rounded">Overall</button>
         <button className="px-4 py-2 bg-blue-500 text-white rounded">NEXRAD</button>
         <button className="px-4 py-2 bg-blue-500 text-white rounded">Satellite</button>
       </div>
-
+      
       {/* Slider on the Left for Flight Level */}
       <FlightLevelSlider />
 
@@ -21,7 +20,9 @@ export default function Home() {
       <TimeSlider />
       
       {/* Map */}
-      <MapboxExample /> 
+      <div className="w-1/2 h-[50vh] mb-4"> 
+        <MapboxExample />
+      </div>
 
 
     </div>
--- webapp/components/Map.tsx
+++ webapp/components/Map.tsx
@@ -1,49 +1,30 @@
-"use client";
+"use client"
 import React, { useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
+
 import 'mapbox-gl/dist/mapbox-gl.css';
-
-
 
 const MapboxExample = () => {
   const mapContainerRef = useRef();
   const mapRef = useRef();
 
   useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhcmxvdHRldjIxIiwiYSI6ImNtOTFwMXRweDAzaGYybG9kb2VjMmc4djIifQ.8ZGFYo70vsKGK6dyTIJ-QQ';
+    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN; 
 
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
-      maxZoom: 5.99,
-      minZoom: 4,
-      zoom: 5,
-      center: [-75.789, 41.874],
-      style: 'mapbox://styles/mapbox/dark-v11'
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9 // starting zoom
     });
+  });
 
-//     mapRef.current.on('load', () => {
-//       mapRef.current.addSource('radar', {
-//         type: 'image',
-//         url: 'https://docs.mapbox.com/mapbox-gl-js/assets/radar.gif',
-//         coordinates: [
-//           [-80.425, 46.437],
-//           [-71.516, 46.437],
-//           [-71.516, 37.936],
-//           [-80.425, 37.936]
-//         ]
-//       });
-//       mapRef.current.addLayer({
-//         id: 'radar-layer',
-//         type: 'raster',
-//         source: 'radar',
-//         paint: {
-//           'raster-fade-duration': 0
-//         }
-//       });
-//     });
-  }, []);
-
-  return <div id="map" ref={mapContainerRef} style={{ height: '100%' }}></div>;
+  return (
+    <div
+      style={{ height: '100%' }}
+      ref={mapContainerRef}
+      className="map-container"
+    />
+  );
 };
 
 export default MapboxExample;
